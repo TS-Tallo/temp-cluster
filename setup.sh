d736cd066010dc2a5025d973545dc578edb96ec6
Tallo <77656358+TS-Tallo@users.noreply.github.com>
2025-05-28 18:49:34 -0400

    Update setup.sh
    Nvidia + Docker fixes

--- setup.sh
+++ setup.sh
@@ -7,9 +7,7 @@ echo "deb [signed-by=/usr/share/keyrings/GPG-KEY-Mellanox.gpg] https://linux.mel
 sudo apt-get install mlnx-fw-updater mlnx-ofed-all -y
 
 # Setup CUDA
-wget -O /tmp/cuda-keyring_1.1-1_all.deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-keyring_1.1-1_all.deb
-sudo dpkg -i /tmp/cuda-keyring_1.1-1_all.deb
-sudo apt-get install cuda-drivers-555 nvidia-kernel-open-555 linux-tools-$(uname -r) -y
+sudo ubuntu-drivers install
 
 # Nvidia Toolkit
 sudo apt install cuda-toolkit nvidia-container-toolkit cudnn libnccl2 nvidia-gds libnccl-dev -y
@@ -19,3 +17,20 @@ sudo apt-get install libnccl2 libnccl-dev -y
 sudo apt-get install libnvidia-compute-560:i386 libnvidia-decode-560:i386 \
  libnvidia-encode-560:i386 libnvidia-extra-560:i386 libnvidia-fbc1-560:i386 \
  libnvidia-gl-560:i386 -y
+
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+# Install Docker
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
