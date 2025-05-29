0b3899ec9952a9491141b1b4643cff3aa3092e63
Tallo <77656358+TS-Tallo@users.noreply.github.com>
2025-05-29 13:25:43 -0400

    Create nvidia.sh

---
+++ nvidia.sh
@@ -0,0 +1,5 @@
+# Setup CUDA
+sudo apt install pkg-config libglvnd-dev dkms build-essential libegl-dev libegl1 libgl-dev libgl1 libgles-dev libgles1 libglvnd-core-dev libglx-dev libopengl-dev gcc make 
+sudo add-apt-repository ppa:graphics-drivers/ppa
+sudo apt update 
+sudo apt install nvidia-driver-555
