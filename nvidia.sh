d3ad2f5b7398c936604f5f29a3a0b829834688ff
Tallo <77656358+TS-Tallo@users.noreply.github.com>
2025-05-29 14:48:32 -0400

    Update nvidia.sh

--- nvidia.sh
+++ nvidia.sh
@@ -2,4 +2,4 @@
 sudo apt install pkg-config libglvnd-dev dkms build-essential libegl-dev libegl1 libgl-dev libgl1 libgles-dev libgles1 libglvnd-core-dev libglx-dev libopengl-dev gcc make 
 sudo add-apt-repository ppa:graphics-drivers/ppa
 sudo apt update 
-sudo apt install nvidia-driver-555
+sudo apt install nvidia-driver-570
